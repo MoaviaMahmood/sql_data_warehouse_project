331ef1bd08f391ce1cc00d7a1933147d3256867c
Moavia Mahmood <168455506+MoaviaMahmood@users.noreply.github.com>
2025-04-13 19:26:43 +0300

    Update bronze_layer_bulk_load.sql

--- Scripts/Bronze/bronze_layer_bulk_load.sql
+++ Scripts/Bronze/bronze_layer_bulk_load.sql
@@ -1,6 +1,6 @@
 TRUNCATE TABLE bronze.crm_cust_info;
 BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\cust_info.csv'
+		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\cust_info.csv' --Update the file path to match the directory structure on your local machine
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -9,7 +9,7 @@ BULK INSERT bronze.crm_cust_info
 
 TRUNCATE TABLE bronze.crm_prd_info;
 BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\prd_info.csv'
+		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\prd_info.csv' --Update the file path to match the directory structure on your local machine
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -18,7 +18,7 @@ BULK INSERT bronze.crm_prd_info
 
 TRUNCATE TABLE bronze.crm_sales_details;
 BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\sales_details.csv' --Update the file path to match the directory structure on your local machine
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -27,7 +27,7 @@ BULK INSERT bronze.crm_sales_details
 
 TRUNCATE TABLE bronze.crm_sales_details;
 BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\sales_details.csv'
+		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_crm\sales_details.csv' --Update the file path to match the directory structure on your local machine
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -36,7 +36,7 @@ BULK INSERT bronze.crm_sales_details
 
 TRUNCATE TABLE bronze.erp_cust_az12;
 BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_erp\cust_az12.csv'
+		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_erp\cust_az12.csv' --Update the file path to match the directory structure on your local machine
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -45,7 +45,7 @@ BULK INSERT bronze.erp_cust_az12
 
 TRUNCATE TABLE bronze.erp_loc_a101;
 BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_erp\loc_a101.csv'
+		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_erp\loc_a101.csv' --Update the file path to match the directory structure on your local machine
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
@@ -54,7 +54,7 @@ BULK INSERT bronze.erp_loc_a101
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'C:\Users\HP Laptop 15\Documents\sql projects\datasets\source_erp\px_cat_g1v2.csv' --Update the file path to match the directory structure on your local machine
 		WITH (
 			FIRSTROW = 2,
 			FIELDTERMINATOR = ',',
